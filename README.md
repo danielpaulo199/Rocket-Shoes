740da7a863ea0264dac69f2c4f8014a1878ad645
Daniel Paulo <Danielpaulo199@gmail.com>
2020-05-20 13:44:55 -0300

    Add: Router

--- README.md
+++
@@ -1,8 +0,0 @@
-# empty-react-project
-Repositório para iniciar um novo projeto utilizando react.
-
-## Conteúdo
-  > React<br>
-  > Eslint<br>
-  > Prettier<br>
-  > Editor Config<br>
---
+++ src/pages/Repository/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Repository() {
+    return <h1>Pages/repository</h1>;
+}
+
+export default Repository;
