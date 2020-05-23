e74d5227bf6e5d0e8b3d047b064b75e5413a7bbb
Daniel Paulo <Danielpaulo199@gmail.com>
2020-05-23 18:16:55 -0300

    First Commit

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Content } from './styles';
+import Logo from '../../assets/logo.png';
 
 function Main() {
-    return <h1>Pages/main</h1>;
+  return (
+    <Container>
+      <Content>
+        <img src={Logo} alt="FastFeet Logo" />
+        <form>
+          <span>SEU E-MAIL</span>
+          <input type="text"></input>
+          <span>SUA SENHA</span>
+          <input type="password"></input>
+          <button type="submit">Entrar no Sistema</button>
+        </form>
+      </Content>
+    </Container>
+  );
 }
 
 export default Main;
