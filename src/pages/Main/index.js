0b7ef5ba6fe0cf4e9800148326b220654ff1f16d
Daniel Paulo <Danielpaulo199@gmail.com>
2020-05-25 15:22:01 -0300

    First Commit

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 //import { Container } from './styles';
 
 function Main() {
-  return <h1>óla main page</h1>;
+  return <h1>óla main page RocketShoes</h1>;
 }
 
 export default Main;
